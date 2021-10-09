13e69acb15bd6218fad003cc4d67a5553e225057
mariana <maritaborue@gmail.com>
2021-10-08 19:48:57 -0500

    remasterizacion

---
+++ src/Components/Main.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Main() {
+    return (
+        <div>
+             <main>
+                 <form action="">
+                     <label htmlFor="nombre">Escribe tu nombre :</label>
+                    <br />
+                     <input type="text" name="nombre" />
+                     <br />
+                     <input type="button" value="Cliqueame" />
+                 </form>
+             </main>
+        </div>
+    )
+}
+
+export default Main
---
+++ src/Components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Navbar() {
+    return (
+        <div>
+            <nav>
+        Barra de Navegacion
+      </nav>
+        </div>
+    )
+}
+
+export default Navbar
